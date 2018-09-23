0b4f5faf92e6df7178949d47e1d1724340365759
Dmitry Mikhalkin <dmitry_mihalkin@mail.ru>
2018-09-23 22:21:50 +0300

    Верстка нового макета

---
+++ scripts/app.js
@@ -0,0 +1,132 @@
+jQuery(document).ready(function () {
+    jQuery('.leftText').addClass("hiddenn").viewportChecker({
+        classToAdd: 'visiblee animated slideInLeft',
+        repeat: true,
+    });
+
+    jQuery('.rightText').addClass("hiddenn").viewportChecker({
+        classToAdd: 'visiblee animated slideInRight',
+        repeat: true,
+    });
+
+    jQuery('.unit1').addClass("hiddenn").viewportChecker({
+        classToAdd: 'visiblee animated rubberBand',
+        repeat: true,
+    });
+
+    jQuery('.unit2').addClass("hiddenn").viewportChecker({
+        classToAdd: 'visiblee animated rubberBand',
+        repeat: true,
+    });
+
+    jQuery('.unit3').addClass("hiddenn").viewportChecker({
+        classToAdd: 'visiblee animated rubberBand',
+        repeat: true,
+    });
+
+    jQuery('.best_solution').addClass("hiddenn").viewportChecker({
+        classToAdd: 'visiblee animated bounceInLeft',
+        repeat: true,
+    });
+
+    jQuery('.be_studio').addClass("hiddenn").viewportChecker({
+        classToAdd: 'visiblee animated bounceInUp',
+        repeat: true,
+    });
+
+    jQuery('.psd_freebies').addClass("hiddenn").viewportChecker({
+        classToAdd: 'visiblee animated bounceInUp',
+        repeat: true,
+    });
+
+    jQuery('.jone_doe').addClass("hiddenn").viewportChecker({
+        classToAdd: 'visiblee animated bounceInDown',
+        repeat: true,
+    });
+
+    jQuery('.creative_brochure').addClass("hiddenn").viewportChecker({
+        classToAdd: 'visiblee animated zoomIn',
+        repeat: true,
+    });
+
+    jQuery('.gift_voucher').addClass("hiddenn").viewportChecker({
+        classToAdd: 'visiblee animated bounceInDown',
+        repeat: true,
+    });
+
+    jQuery('.client').addClass("hiddenn").viewportChecker({
+        classToAdd: 'visiblee animated flash',
+        repeat: true,
+    });
+
+    jQuery('.unicorns').addClass("hiddenn").viewportChecker({
+        classToAdd: 'visiblee animated rotateInDownLeft',
+        repeat: true,
+    });
+
+    jQuery('.atlantis').addClass("hiddenn").viewportChecker({
+        classToAdd: 'visiblee animated rotateInUpLeft',
+        repeat: true,
+    });
+
+    jQuery('.great_falls').addClass("hiddenn").viewportChecker({
+        classToAdd: 'visiblee animated rotateInUpRight',
+        repeat: true,
+    });
+
+    jQuery('.norsemen').addClass("hiddenn").viewportChecker({
+        classToAdd: 'visiblee animated rotateInDownRight',
+        repeat: true,
+    });
+
+    jQuery('.blog_wrap').addClass("hiddenn").viewportChecker({
+        classToAdd: 'visiblee animated rotateInDownRight',
+        repeat: true,
+    });
+
+    jQuery('.november_11_blue').addClass("hiddenn").viewportChecker({
+        classToAdd: 'visiblee animated rotateInDownRight',
+        repeat: true,
+    });
+
+    jQuery('.november_11_white').addClass("hiddenn").viewportChecker({
+        classToAdd: 'visiblee animated rotateInDownRight',
+        repeat: true,
+    });
+
+    jQuery('.november_11_black').addClass("hiddenn").viewportChecker({
+        classToAdd: 'visiblee animated rotateInDownRight',
+        repeat: true,
+    });
+
+    jQuery('.november_11_with_background').addClass("hiddenn").viewportChecker({
+        classToAdd: 'visiblee animated rotateInDownRight',
+        repeat: true,
+    });
+
+    $('.november_11_pink').addClass("hiddenn").viewportChecker({
+        classToAdd: 'visiblee animated rotateInDownRight',
+        repeat: true,
+    });
+
+    $('.get_in_touch').addClass("hiddenn").viewportChecker({
+        classToAdd: 'visiblee animated bounceIn',
+        repeat: true,
+    });
+
+    $('.post').addClass("hiddenn").viewportChecker({
+        classToAdd: 'visiblee animated bounceIn',
+        repeat: true,
+    });
+
+    $('.phones').addClass("hiddenn").viewportChecker({
+        classToAdd: 'visiblee animated bounceIn',
+        repeat: true,
+    });
+
+    $('.map').addClass("hiddenn").viewportChecker({
+        classToAdd: 'visiblee animated bounceIn',
+        repeat: true,
+    });
+
+});
